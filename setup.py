17c2d30be54627fbd9f09d856ea97840bec03ef6
Dmitry Popov <77672371+MitryP@users.noreply.github.com>
2021-06-17 18:19:46 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
   name = 'datify',
   packages = setuptools.find_packages(),
-  version = '0.91',
+  version = '1.01',
   license='Apache-2.0',
   description = 'Module that allows extracting valid date from user input.',
   long_description=long_description,
@@ -15,7 +15,7 @@ setuptools.setup(
   author = 'Dmitry Popov',
   author_email = 'thedmitryp@ukr.net',
   url = 'https://github.com/MitryP/datify',
-  download_url = 'https://github.com/MitryP/datify/archive/refs/tags/0.91.tar.gz',
+  download_url = 'https://github.com/MitryP/datify/archive/refs/tags/1.01.tar.gz',
   keywords = ['str', 'string', 'user-experience', 'user-input', 'date', 'date-strings', 'datify', 'alpha-month', 'month', 'engligh', 'russian', 'ukrainian', 'natural-language', 'open-source'],
   install_requires=[],
   classifiers=[
